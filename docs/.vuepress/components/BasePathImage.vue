8d5c49ec7f475010c40b3e0863845cceca816d41
Vinícius Ueda <vnsueda@gmail.com>
2020-05-11 20:25:04 -0300

    Imagens com base path configurado

---
+++ docs/.vuepress/components/BasePathImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="$withBase(src)" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
